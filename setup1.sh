eeb2ebd111318ca302a9b58ac10deeddf9eb4f16
Adekoreday <khord4eng@gmail.com>
2020-03-14 13:45:07 +0100

    enable script to print todays date

--- setup1.sh
+++ setup1.sh
@@ -8,3 +8,8 @@ echo enter the name of the executable file
 read filename
 mv 1.sh $filename.sh
 chmod +x $filename.sh
+echo " DONE !!"
+#lets make this script list its current directory and a calendar with todays date
+
+echo " pwd " >> $filename.sh
+echo " cal " >> $filename.sh
